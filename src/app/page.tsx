ecf4895ad71693f7116a134f80d825fa6df5d454
Miguel <miguelt2296@gmail.com>
2025-02-21 22:33:01 -0500

    data types ADDED

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,39 @@
 let txButton:string = "Haz Click"
 
+//  puedo definir un tipo de dato
+ type ButtonProps = {
+  text : string,
+  subtitle? :  string,
+  color? : string
+ }
+//  Los signos de interrogacion indican que la variable es opcional
+
+
 function suma(x: number,y:number) : number{
   return x + y;
 }
 
 
-function Button(){
+function Button(props: ButtonProps){
   console.log("El numero es: ",suma(1,2));
+
+  // Podria extraer cada variable y asignarla
+  const {text, subtitle, color} = props
+
+
   return(
-    <button>{txButton}</button>
+    <>
+      <button>{txButton}</button>
+      <button>{text}</button>
+      <button>{subtitle}</button>
+      <button>{color}</button>
+    </>
   )
-}
+} 
 
 function Page(){
   return(
-  <Button />
+  <Button text = "Hello World" subtitle = "Este es el subtitulo" color = "Red" />
   ) 
 }
 
