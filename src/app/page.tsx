c6be9342589990bd582a56e8d7eedd41979bf6ea
Miguel <miguelt2296@gmail.com>
2025-02-23 10:32:20 -0500

    Pasar arreglos a los props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,22 @@
-let txButton:string = "Haz Click"
-
-// Puedo crear un type que puedo asignarselo a otro tipo de dato
-
-type Color = "Red" | "Green" | "Blue"
 
 //  puedo definir un tipo de dato
  type ButtonProps = {
-  text : string,
-  subtitle? :  string,
-  color? : Color,
-  // Puedo especificar que opciones son posibles
-  backgroundColor? : Color | "Yellow"
+  padding : [number,number,number?,number?],
  }
 //  Los signos de interrogacion indican que la variable es opcional
 
 
-function suma(x: number,y:number) : number{
-  return x + y;
-}
-
-
-function Button(props: ButtonProps){
-  console.log("El numero es: ",suma(1,2));
-
-  // Podria extraer cada variable y asignarla
-  const {text, subtitle, color, backgroundColor} = props
-
-
+function Button({ } : ButtonProps){
   return(
     <>
-      <button>{txButton}</button>
-      <button>{text}</button>
-      <button>{subtitle}</button>
-      <button>{color}</button> 
-      <button>{backgroundColor}</button> 
+      <button>padding</button>
     </>
   )
 } 
 
 function Page(){
   return(
-  <Button text = "Hello World" subtitle = "Este es el subtitulo" color = "Red" backgroundColor="Yellow"/>
+  <Button padding = {[22,23,24,15]}/>
   ) 
 }
 
